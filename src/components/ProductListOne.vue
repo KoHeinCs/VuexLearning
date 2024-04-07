0bf807a4484176be12f5f33b720e42e03b5c99a2
Ko Hein <heinhtetaungcu@gmail.com>
2024-04-07 10:23:42 +0630

    - modify store.js   => set strict:true because we don't allow to modify store's state directly from components   => set devtools:true because we can use vue devtools in browser
    (1) Learn vuex's [getters] properties to get saleProducts
        - we can get store's state directly from component via $store.state.products
        - we can also get store's state directly from component via $store.getters
        - ** Note ** we use store's [getters] properties
          if we need to select state from multiple components
    
    (2) Learn vuex's [mutations] properties
        - we use [mutations] to modify store's state
        - [mutations] should be the only way to modify store's state
        - we can modify [mutations] via $store.commit
        - we should not use [mutations] directly from component even though component can commit that [mutations] directly
          because it is hard to debug [mutations] actions if there is asynchronous  tasks such as data fetching
        - store's [actions] properties should be used to commit that [mutations] properties
    
    (3) Learn vuex's [actions] properties
        - store's [actions] properties should be used to commit that [mutations] properties because it is easy to debug if there is asynchronous  tasks such as data fetching
        - components can dispatch to [actions] properties via $store.dispatch to modify store's state

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -1,73 +1,91 @@
 <template>
-    <div id="product-list-one">
-        <h2>
-            Product List One
-        </h2>
-        <ul>
-            <li v-for="product in products">
-                <span class="name">{{ product.name }}</span>
-                <span class="price"> ${{ product.price }}</span>
-            </li>
-        </ul>
-    </div>
+  <div id="product-list-one">
+    <h1>
+      {{ message }}
+    </h1>
+    <h2>
+      Product List One
+    </h2>
+    <ul>
+      <li v-for="product in saleProducts">
+        <span class="name">{{ product.name }}</span>
+        <span class="price"> ${{ product.price }}</span>
+      </li>
+    </ul>
+    <button @:click="reducePrice">Reduce Price</button>
+  </div>
 </template>
 
 <script>
 
 export default {
-    name: 'ProuductListOne',
-    computed: {
-        products() {
-            return this.$store.state.products;
-        }
+  name: 'ProuductListOne',
+  data() {
+    return {
+      message: "Ready To Learn Vuex "
     }
+  },
+  computed: {
+    products() {
+      return this.$store.state.products;
+    },
+    saleProducts() {
+      return this.$store.getters.saleProducts;
+    }
+  },
+  methods:{
+    reducePrice(){
+      //return this.$store.commit('reducePrice',1)
+     this.$store.dispatch('reducePrice',1);
+    }
+  }
 }
 </script>
 
 <style scoped>
 /* Styling for the product list container */
 #product-list-one {
-    background-color: #f7f7f7;
-    border-radius: 8px;
-    padding: 20px;
-    margin: 20px 0;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: #f7f7f7;
+  border-radius: 8px;
+  padding: 20px;
+  margin: 20px 0;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
 }
 
 /* Styling for the heading */
 #product-list-one h2 {
-    color: #333;
-    font-size: 24px;
-    margin-bottom: 10px;
+  color: #333;
+  font-size: 24px;
+  margin-bottom: 10px;
 }
 
 /* Styling for the list */
 #product-list-one ul {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-    gap: 20px;
-    padding: 0;
-    list-style: none;
-    /* Remove bullet points */
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 20px;
+  padding: 0;
+  list-style: none;
+  /* Remove bullet points */
 }
 
 /* Styling for each list item */
 #product-list-one li {
-    padding: 15px;
-    border: 1px solid #ddd;
-    border-radius: 8px;
-    background-color: #fff;
+  padding: 15px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  background-color: #fff;
 }
 
 /* Styling for product name */
 #product-list-one .name {
-    font-weight: bold;
-    color: #333;
+  font-weight: bold;
+  color: #333;
 }
 
 /* Styling for product price */
 #product-list-one .price {
-    margin-left: 10px;
-    color: #555;
+  margin-left: 10px;
+  color: #555;
 }
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,37 @@
-
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
 
 const store = createStore({
     state() {
         return {
             products: [
-                { name: 'Bana Skin', price: 20 },
-                { name: 'Shiny Star', price: 40 },
-                { name: 'Green Shells', price: 60 },
-                { name: 'Red Shells', price: 80 }
+                {name: 'Bana Skin', price: 20},
+                {name: 'Shiny Star', price: 40},
+                {name: 'Green Shells', price: 60},
+                {name: 'Red Shells', price: 80}
             ]
         }
 
+    },
+    devtools: true,
+    strict:true,
+    getters: {
+        saleProducts: state => state.products.map(product => {
+            return {
+                name: '**' + product.name + '**',
+                price: product.price / 2
+            }
+        })
+    },
+    mutations:{
+        reducePrice : (state,payload) => state.products.forEach(product => product.price -= payload)
+    },
+    actions:{
+        reducePrice: (store,payload) => {
+            setTimeout(function (){
+                store.commit('reducePrice',payload)
+            },2000)
+
+        }
     }
 })
 
