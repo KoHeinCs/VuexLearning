4ebc935880ef6c7608e4ebe46e9fbce9ba124ff3
Ko Hein <heinhtetaungcu@gmail.com>
2024-04-10 13:44:01 +0630

    - refactor product module

--- src/store/modules/product/index.js
+++ src/store/modules/product/index.js
@@ -1,6 +1,6 @@
 import {createStore} from 'vuex'
 
-import {products} from './states'
+import productsState from './states'
 import * as getters from './getters'
 import * as mutations from './mutations'
 import * as actions from './actions'
@@ -9,7 +9,7 @@ import * as actions from './actions'
 const index = createStore({
     devtools: true,
     strict: true,
-    state:{products},
+    state:productsState,
     getters,
     mutations,
     actions
--- src/store/modules/product/states.js
+++ src/store/modules/product/states.js
@@ -1,7 +1,15 @@
 
-export const products =  [
-    {name: 'Bana Skin', price: 20},
-    {name: 'Shiny Star', price: 40},
-    {name: 'Green Shells', price: 60},
-    {name: 'Red Shells', price: 80}
-]
+
+const productsState = {
+    products: [
+        {name: 'Bana Skin', price: 20},
+        {name: 'Shiny Star', price: 40},
+        {name: 'Green Shells', price: 60},
+        {name: 'Red Shells', price: 80}
+    ]
+}
+
+
+export default productsState;
+
+
